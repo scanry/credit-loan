dcf3143351873783a08f241f63dd350be1d95967
sixliu <359852326@qq.com>
2018-06-15 17:25:58 +0800

    新增运营管理应用

---
+++ operate-manager/src/test/java/com/sixliu/credit/operate/BaseTest.java
@@ -0,0 +1,32 @@
+package com.sixliu.credit.operate;
+
+import java.util.TimeZone;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+/**
+ * @author:MG01867
+ * @date:2018年2月5日
+ * @E-mail:359852326@qq.com
+ * @version:
+ * @describe 
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StartUp.class,webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public abstract class BaseTest extends AbstractJUnit4SpringContextTests {
+	
+	protected static final Logger log = LoggerFactory.getLogger(BaseTest.class);
+	
+	static {
+		final TimeZone zone = TimeZone.getTimeZone("GMT+8"); // 获取中国时区
+		TimeZone.setDefault(zone); // 设置时区
+	}
+
+}
