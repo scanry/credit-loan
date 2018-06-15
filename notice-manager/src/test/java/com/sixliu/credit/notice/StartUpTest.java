bbfe305f4ec703da382bfcbc23bfefdd30324861
sixliu <359852326@qq.com>
2018-06-15 17:05:06 +0800

    新增贷款通知服务

---
+++ notice-manager/src/test/java/com/sixliu/credit/notice/StartUpTest.java
@@ -0,0 +1,23 @@
+package com.sixliu.credit.notice;
+
+import org.junit.Test;
+
+/**
+*@author:MG01867
+*@date:2018年6月15日
+*@E-mail:359852326@qq.com
+*@version:
+*@describe //TODO
+*/
+public class StartUpTest extends BaseTest {
+
+	@Test
+	public void test() {
+//		synchronized (this) {
+//			try {
+//				wait();
+//			} catch (InterruptedException e) {
+//			}
+//		}
+	}
+}
