be618020986ee3dab9f80cc8e377508399d92b29
Administrator <Administrator@192.168.1.102>
2018-06-16 14:36:06 +0800

    更新

---
+++ product-manager/src/main/java/com/sixliu/credit/product/repository/po/ProductApplyPrecheckConfig.java
@@ -0,0 +1,20 @@
+package com.sixliu.credit.product.repository.po;
+
+import com.sixliu.credit.common.po.BasePo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author: sixliu
+ * @email: 359852326@qq.com
+ * @date: 2018年6月16日 下午2:26:58
+ * @version V1.0
+ * @Description:TODO
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ProductApplyPrecheckConfig extends BasePo {
+	
+}
