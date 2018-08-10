36a1ef785b3d4b853863638c8603508451d2b7ab
sixliu <359852326@qq.com>
2018-08-10 18:36:54 +0800

    更新

---
+++ user-manager-api/src/main/java/com/sixliu/credit/user/App.java
@@ -0,0 +1,13 @@
+package com.sixliu.credit.user;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
